42046523463fcb42ffb9ab2261f23d283318cc45
Mark Du <dulongjiebackup@gmail.com>
2023-08-15 11:49:12 -0400

    add aftersignout url to make closed loop, dialog

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,11 @@
+import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 
-export default function Home() {
+export default function SetupPage() {
   return (
-    <h2>home page</h2>
+    <div className='p-4'>
+      <UserButton afterSignOutUrl='/'/>
+      this is root route
+    </div>
   )
 }
