8f28b8871b9f7b6d3216c72122023d714a386a19
Mark Du <dulongjiebackup@gmail.com>
2023-08-21 11:39:48 -0400

    category model, copy categories from billboard

---
+++ app/(dashboard)/[storeId]/(routes)/categories/components/category-client.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import { useParams, useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { ApiList } from "@/components/shared/api-list";
+
+import { columns, CategoryColumn } from "./CategoryColumn";
+import { Plus } from "lucide-react";
+import { DataTable } from "@/components/ui/data-table";
+
+
+
+interface Props {
+  data: CategoryColumn[];
+}
+
+export const CategoryClient: React.FC<Props> = ({
+  data
+}) => {
+  const params = useParams();
+  const router = useRouter();
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading title={`Categories (${data.length})`} description="Manage categories for your store" />
+        <Button onClick={() => router.push(`/${params.storeId}/categories/new`)}>
+          <Plus className="mr-2 h-4 w-4" /> Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable searchKey="label" columns={columns} data={data} />
+      <Heading title="API" description="API Calls for Categories" />
+      <Separator />
+      <ApiList entityName="categories" entityIdName="categoryId" />
+    </>
+  );
+};
