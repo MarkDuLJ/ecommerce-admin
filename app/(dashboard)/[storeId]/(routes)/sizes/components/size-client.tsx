9e2580939db251bbcf781943bec7a7f04f0883c7
Mark Du <dulongjiebackup@gmail.com>
2023-08-22 12:01:29 -0400

    finish size api/frontend

---
+++ app/(dashboard)/[storeId]/(routes)/sizes/components/size-client.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import { useParams, useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { ApiList } from "@/components/shared/api-list";
+
+import { columns, SizeColumn } from "./columns";
+import { Plus } from "lucide-react";
+import { DataTable } from "@/components/ui/data-table";
+
+
+
+interface Props {
+  data: SizeColumn[];
+}
+
+export const SizeClient: React.FC<Props> = ({
+  data
+}) => {
+  const params = useParams();
+  const router = useRouter();
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading title={`Sizes (${data.length})`} description="Manage sizes for your store" />
+        <Button onClick={() => router.push(`/${params.storeId}/sizes/new`)}>
+          <Plus className="mr-2 h-4 w-4" /> Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable searchKey="name" columns={columns} data={data} />
+      <Heading title="API" description="API Calls for Sizes" />
+      <Separator />
+      <ApiList entityName="sizes" entityIdName="sizeId" />
+    </>
+  );
+};
